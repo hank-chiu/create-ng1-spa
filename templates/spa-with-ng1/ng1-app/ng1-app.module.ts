14593bbd815a7887a01ffb9db0cf0e6367d0e0ac
hank <hankchiu.tw@gmail.com>
2023-06-02 17:39:44 +0800

    feat: apply APP_ROOT_COMP_KEY

--- templates/spa-with-ng1/ng1-app/ng1-app.module.ts
+++ templates/spa-with-ng1/ng1-app/ng1-app.module.ts
@@ -1,3 +1,5 @@
+import { APP_ROOT_COMP_KEY } from 'Feature/ng1-migration';
+
 import { DowngradeModule } from '../downgrade.module';
 import { Ng1AppController } from './ng1-app.controller';
 import template from './ng1-app.html';
@@ -6,8 +8,4 @@ declare const angular: any;
 
 export const appModule = angular
   .module('{{ng1ModuleName}}', ['ngRoute', 'hcApp', DowngradeModule.moduleName])
-  .component('ng1AppRoot', {
-    controller: Ng1AppController,
-    controllerAs: 'vm',
-    template,
-  });
+  .value(APP_ROOT_COMP_KEY, { controller: Ng1AppController, template });
